ad1cf0a913b6fcd012d9c2117e36e0fde69a0455
阿正 <820108271@qq.com>
2026-07-20 10:56:48 +0800

    feat 调整k8sConfig

--- nanobot/agent/tools/k8s/env.py
+++ nanobot/agent/tools/k8s/env.py
@@ -7,15 +7,11 @@ KUBE_CONFIG_DIR = Path(__file__).parent / "kube_config"
 KubeConfigPath = [
     {
         'absolute_path': f'{KUBE_CONFIG_DIR / '192.168.81.7'}',
-        'alias': ['192.168.81.7', 'Code&One压测环境']
+        'alias': ['192.168.81.7', 'Code&One&Scan压测环境']
     },
     {
         'absolute_path': f'{KUBE_CONFIG_DIR / '192.168.90.17'}',
         'alias': ['192.168.90.17', 'Repo&Wiki压测环境']
-    },
-    {
-        'absolute_path': f'{KUBE_CONFIG_DIR / '192.168.48.100'}',
-        'alias': ['192.168.48.100', 'POC高可用测试环境']
     },
 
 ]
